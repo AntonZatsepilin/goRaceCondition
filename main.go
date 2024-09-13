d8dd681c200bdbfdd96194d324b4761c96e03abc
AntonZatsepilin <zatsepilin90.00@bk.ru>
2024-09-13 14:20:09 +0300

    raceConditionErr

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	counter := 0
+	for i := 0; i < 1000; i++ {
+		go func() {
+			counter++
+		}()
+	}
+	time.Sleep(time.Second * 2)
+	fmt.Println(counter)
+}
